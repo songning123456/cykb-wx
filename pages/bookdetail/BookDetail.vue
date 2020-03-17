4a9416cac56c0d3f9bda62bd66b368bb60a193ac
songning <songning@kedacom.com>
2020-03-17 17:27:40 +0800

    BookDetail 初步调试

--- pages/bookdetail/BookDetail.vue
+++ pages/bookdetail/BookDetail.vue
@@ -1,16 +1,59 @@
 <template>
     <view class="book-detail full-screen">
-        <view class="cu-card case no-card">
+        <view class="cu-card case no-card margin-bottom-sm">
             <view class="cu-item shadow">
                 <view class="image text-center">
                     <uni-image class="cu-avatar image-size" :url="novels.coverUrl"></uni-image>
                     <view class="margin-top-xs text-gray text-df">{{novels.title}}</view>
                     <view class="margin-top-xs">
                         <view class="cu-tag bg-red light sm round">{{novels.author}}</view>
-                        <view class="cu-tag bg-green light sm round">{{convertCategory(novels.sex, novels.category)}}</view>
+                        <view class="cu-tag bg-green light sm round">{{convertCategory(novels.sex, novels.category)}}
+                        </view>
                         <view class="cu-tag bg-yellow light sm round">{{convertSex(novels.sex)}}</view>
                     </view>
                 </view>
+            </view>
+        </view>
+        <view class="cu-card article no-card margin-bottom-sm bg-white">
+            <view class="title text-center text-bold padding-top-xs">
+                <text>简介</text>
+            </view>
+            <view class="content padding">
+                {{novels.introduction}}
+            </view>
+        </view>
+        <view class="cu-list menu">
+            <view class="cu-item">
+                <view class="content">
+                    <text class="text-black">最新章节</text>
+                </view>
+                <view class="action text-cut">
+                    {{novels.latestChapter}}
+                </view>
+            </view>
+            <view class="cu-item">
+                <view class="content">作者还写过</view>
+                <view class="action text-gray">更多</view>
+            </view>
+            <scroll-view scroll-x="true" class="author-books">
+                <block v-for="(item, index) in authorBooks" :key="index">
+                    <view class="author-book" @tap="">
+                        <view class="img">
+                            <uni-image :url="item.coverUrl" class="uni-image"></uni-image>
+                        </view>
+                        <text class="item-title">{{item.title}}</text>
+                    </view>
+                </block>
+            </scroll-view>
+            <view class="cu-item">
+                <view class="content">同类型推荐</view>
+                <view class="action text-gray">更多</view>
+            </view>
+        </view>
+        <view class="bottom-bar bg-white">
+            <view class="btn-group cu-bar tabbar">
+                <button class="cu-btn text-red line-red shadow">加入书架</button>
+                <button class="cu-btn bg-red shadow-blur">开始阅读</button>
             </view>
         </view>
     </view>
@@ -22,19 +65,21 @@
 
     export default {
         name: 'BookDetail',
-        data () {
+        data() {
             return {
-                novels: null
+                novels: null,
+                authorBooks: [],
+                similarBooks: []
             }
         },
-        onLoad (option) {
+        onLoad(option) {
             this.novels = JSON.parse(option.novels)
         },
         methods: {
-            convertSex (sex) {
+            convertSex(sex) {
                 return common.getSex(sex)
             },
-            convertCategory (sex, category) {
+            convertCategory(sex, category) {
                 return Category[sex][category]
             },
         }
@@ -45,7 +90,7 @@
     .book-detail {
         .cu-card {
             width: 100%;
-            height: 360upx;
+            max-height: 360upx;
 
             .cu-item {
                 width: 100%;
@@ -54,7 +99,7 @@
                 .image {
                     width: 100%;
                     height: 100%;
-                    padding: 30upx 30upx 0 30upx;
+                    padding: 30upx;
                     box-sizing: border-box;
                     margin: unset;
 
@@ -69,6 +114,45 @@
                     }
                 }
             }
+        }
+
+        .author-books {
+            white-space: nowrap; // 滚动必须加的属性
+            width: 100%;
+            height: 280upx;
+            padding: 20upx;
+            margin: 0 auto;
+
+            .author-book {
+                width: 24%;
+                margin-right: 20upx;
+                display: inline-block;
+                vertical-align: top;
+
+                .img {
+                    display: inline-block;
+
+                    .uni-image {
+                        width: 170upx;
+                        height: 220upx;
+                        border-radius: 6upx;
+                    }
+                }
+
+                .item-title {
+                    display: block; // 让字体换行
+                    width: 90%;
+                    font-size: 30upx;
+                    line-height: 40upx;
+                }
+            }
+        }
+
+        .bottom-bar {
+            bottom: 0;
+            position: fixed;
+            width: 100%;
+            border-top: 1px solid grey;
         }
     }
 
--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -1674,9 +1674,9 @@ function normalizeComponent (
 /***/ }),
 
 /***/ 15:
-/*!*****************************************!*\
-  !*** C:/project/cykb-wx/store/store.js ***!
-  \*****************************************/
+/*!*************************************************!*\
+  !*** D:/WebStormProject/cykb-wx/store/store.js ***!
+  \*************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8686,9 +8686,9 @@ internalMixin(Vue);
 /***/ }),
 
 /***/ 23:
-/*!******************************************!*\
-  !*** C:/project/cykb-wx/util/request.js ***!
-  \******************************************/
+/*!**************************************************!*\
+  !*** D:/WebStormProject/cykb-wx/util/request.js ***!
+  \**************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8734,9 +8734,9 @@ request;exports.default = _default;
 /***/ }),
 
 /***/ 24:
-/*!*****************************************!*\
-  !*** C:/project/cykb-wx/util/common.js ***!
-  \*****************************************/
+/*!*************************************************!*\
+  !*** D:/WebStormProject/cykb-wx/util/common.js ***!
+  \*************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8787,9 +8787,9 @@ common;exports.default = _default;
 /***/ }),
 
 /***/ 25:
-/*!*******************************************!*\
-  !*** C:/project/cykb-wx/util/category.js ***!
-  \*******************************************/
+/*!***************************************************!*\
+  !*** D:/WebStormProject/cykb-wx/util/category.js ***!
+  \***************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8862,9 +8862,9 @@ module.exports = g;
 /***/ }),
 
 /***/ 4:
-/*!*************************************!*\
-  !*** C:/project/cykb-wx/pages.json ***!
-  \*************************************/
+/*!*********************************************!*\
+  !*** D:/WebStormProject/cykb-wx/pages.json ***!
+  \*********************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8874,9 +8874,9 @@ module.exports = g;
 /***/ }),
 
 /***/ 48:
-/*!**********************************************!*\
-  !*** C:/project/cykb-wx/util/convertDate.js ***!
-  \**********************************************/
+/*!******************************************************!*\
+  !*** D:/WebStormProject/cykb-wx/util/convertDate.js ***!
+  \******************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -9822,9 +9822,9 @@ module.exports = {"_from":"@dcloudio/uni-stat@next","_id":"@dcloudio/uni-stat@2.
 /***/ }),
 
 /***/ 65:
-/*!******************************************!*\
-  !*** C:/project/cykb-wx/util/regular.js ***!
-  \******************************************/
+/*!**************************************************!*\
+  !*** D:/WebStormProject/cykb-wx/util/regular.js ***!
+  \**************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -9838,21 +9838,21 @@ regular;exports.default = _default;
 /***/ }),
 
 /***/ 7:
-/*!******************************************************!*\
-  !*** C:/project/cykb-wx/pages.json?{"type":"style"} ***!
-  \******************************************************/
+/*!**************************************************************!*\
+  !*** D:/WebStormProject/cykb-wx/pages.json?{"type":"style"} ***!
+  \**************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _default = { "pages": { "pages/navigation/HomePage": { "navigationBarTitleText": "首页", "navigationBarBackgroundColor": "#e54d42", "enablePullDownRefresh": true }, "pages/navigation/Classify": { "navigationBarTitleText": "分类", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true }, "pages/navigation/Search": { "navigationBarTitleText": "搜索", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true }, "pages/navigation/Bookcase": { "navigationBarTitleText": "书架", "navigationBarBackgroundColor": "#e54d42", "enablePullDownRefresh": true }, "pages/navigation/My": { "navigationBarTitleText": "我的", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true }, "pages/comment/Comment": { "navigationBarTitleText": "意见反馈", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true }, "pages/login/Login": { "navigationBarTitleText": "登录", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true }, "pages/result/SearchResult": { "navigationBarTitleText": "", "navigationBarBackgroundColor": "#e54d42", "enablePullDownRefresh": true }, "pages/settings/Settings": { "navigationBarTitleText": "设置", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true }, "pages/bookdetail/BookDetail": { "navigationBarTitleText": "书籍详情", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true } }, "globalStyle": { "navigationBarTextStyle": "white", "navigationBarTitleText": "畅游看吧", "navigationBarBackgroundColor": "#f7f6f2", "backgroundColor": "#f7f6f2" } };exports.default = _default;
+Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _default = { "pages": { "pages/navigation/HomePage": { "navigationBarTitleText": "首页", "navigationBarBackgroundColor": "#e54d42", "enablePullDownRefresh": true, "usingComponents": {}, "usingAutoImportComponents": {} }, "pages/navigation/Classify": { "navigationBarTitleText": "分类", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true, "usingComponents": {}, "usingAutoImportComponents": {} }, "pages/navigation/Search": { "navigationBarTitleText": "搜索", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true, "usingComponents": {}, "usingAutoImportComponents": {} }, "pages/navigation/Bookcase": { "navigationBarTitleText": "书架", "navigationBarBackgroundColor": "#e54d42", "enablePullDownRefresh": true, "usingComponents": { "inset-login": "/components/InsetLogin" }, "usingAutoImportComponents": {} }, "pages/navigation/My": { "navigationBarTitleText": "我的", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true, "usingComponents": {}, "usingAutoImportComponents": {} }, "pages/comment/Comment": { "navigationBarTitleText": "意见反馈", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true, "usingComponents": {}, "usingAutoImportComponents": {} }, "pages/login/Login": { "navigationBarTitleText": "登录", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true, "usingComponents": {}, "usingAutoImportComponents": {} }, "pages/result/SearchResult": { "navigationBarTitleText": "", "navigationBarBackgroundColor": "#e54d42", "enablePullDownRefresh": true, "usingComponents": {}, "usingAutoImportComponents": {} }, "pages/settings/Settings": { "navigationBarTitleText": "设置", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true, "usingComponents": {}, "usingAutoImportComponents": {} }, "pages/bookdetail/BookDetail": { "navigationBarTitleText": "书籍详情", "navigationBarBackgroundColor": "#e54d42", "disableScroll": true, "usingComponents": {}, "usingAutoImportComponents": {} } }, "globalStyle": { "navigationBarTextStyle": "white", "navigationBarTitleText": "畅游看吧", "navigationBarBackgroundColor": "#f7f6f2", "backgroundColor": "#f7f6f2" } };exports.default = _default;
 
 /***/ }),
 
 /***/ 8:
-/*!*****************************************************!*\
-  !*** C:/project/cykb-wx/pages.json?{"type":"stat"} ***!
-  \*****************************************************/
+/*!*************************************************************!*\
+  !*** D:/WebStormProject/cykb-wx/pages.json?{"type":"stat"} ***!
+  \*************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
