f3c179a57381ff52a697676a40493fcdd230aad3
songning <1457065857@qq.com>
2020-03-28 11:12:18 +0800

    版本更新提醒

--- App.vue
+++ App.vue
@@ -4,28 +4,62 @@
             uni.getStorage({
                 key: 'userInfo',
                 success: data => {
-                    this.$store.commit('SET_USERINFO', data.data)
+                    this.$store.commit('SET_USERINFO', data.data);
                 }
-            })
+            });
             uni.getStorage({
                 key: 'sortType',
                 success: data => {
-                    this.$store.commit('SET_SORTTYPE', data.data)
+                    this.$store.commit('SET_SORTTYPE', data.data);
                 }
-            })
+            });
             uni.getSystemInfo({
                 success: function (e) {
-                    uni.setStorageSync('phoneInfo', e)
+                    uni.setStorageSync('phoneInfo', e);
                 }
-            })
+            });
+            // 提醒用户更新版本
+            const updateManager = uni.getUpdateManager();
+            updateManager.onCheckForUpdate(function(res) {
+                // 请求完新版本信息的回调
+                if (res.hasUpdate) {
+                    updateManager.onUpdateReady(function(res2) {
+                        uni.showModal({
+                            title: '更新提示',
+                            content: '发现新版本，是否重启应用?',
+                            cancelColor:'#eeeeee',
+                            confirmColor:'#FF0000',
+                            success(res2) {
+                                if (res2.confirm) {
+                                    // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                                    updateManager.applyUpdate();
+                                }
+                            }
+                        });
+                    });
+                }
+            });
+            updateManager.onUpdateFailed(function(res) {
+                // 新的版本下载失败
+                uni.showModal({
+                    title: '提示',
+                    content: '检查到有新版本，但下载失败，请检查网络设置',
+                    success(res) {
+                        if (res.confirm) {
+                            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                            updateManager.applyUpdate();
+                        }
+                    }
+                });
+            });
         },
         onShow: function () {
-            console.log('App Show')
+            console.log('App Show');
         },
         onHide: function () {
-            console.log('App Hide')
+            console.log('App Hide');
         }
-    }
+    };
 </script>
 
 <style>
--- unpackage/dist/dev/mp-weixin/common/main.js
+++ unpackage/dist/dev/mp-weixin/common/main.js
@@ -118,6 +118,40 @@ __webpack_require__.r(__webpack_exports__);
         uni.setStorageSync('phoneInfo', e);
       } });
 
+    // 提醒用户更新版本
+    var updateManager = uni.getUpdateManager();
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      if (res.hasUpdate) {
+        updateManager.onUpdateReady(function (res2) {
+          uni.showModal({
+            title: '更新提示',
+            content: '发现新版本，是否重启应用?',
+            cancelColor: '#eeeeee',
+            confirmColor: '#FF0000',
+            success: function success(res2) {
+              if (res2.confirm) {
+                // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                updateManager.applyUpdate();
+              }
+            } });
+
+        });
+      }
+    });
+    updateManager.onUpdateFailed(function (res) {
+      // 新的版本下载失败
+      uni.showModal({
+        title: '提示',
+        content: '检查到有新版本，但下载失败，请检查网络设置',
+        success: function success(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate();
+          }
+        } });
+
+    });
   },
   onShow: function onShow() {
     console.log('App Show');
